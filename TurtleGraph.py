4d1a7c91dbcbb2a76253b89a0a1e97a7ef5eb0c4
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2018-11-21 19:51:00 +0900

    Add files via upload

--- TurtleGraph.py
+++ TurtleGraph.py
@@ -9,7 +9,29 @@ class MyTurtle:
         self.windowSizeY = 850
         self.degree = 270
         turtle.setup(self.windowSizeX,self.windowSizeY, 30, 30)
+
+        self.hold_clear = False
+        
         self.turtle = turtle.Turtle()
+        self.grid_turtle = turtle.Turtle()
+        self.text_turtle = turtle.Turtle()
+
+        self.turtle.reset()
+        self.turtle.penup()
+        self.turtle.speed(0)
+        self.turtle.ht()
+        self.turtle.color("blue")
+
+        self.grid_turtle.reset()
+        self.grid_turtle.penup()
+        self.grid_turtle.speed(0)
+        self.grid_turtle.ht()
+
+        self.text_turtle.reset()
+        self.text_turtle.penup()
+        self.text_turtle.speed(0)
+        self.text_turtle.ht()
+        
         #self.turtle.screensize(windowSizeX,windowSizeY)
         self.gridSize = 25
         self.gridNormalSize = 15
@@ -20,9 +42,6 @@ class MyTurtle:
         self.releOrgX = 0 
         self.releOrgY = 0 
         print("initX : %d\ninitY : %d"%(self.releOrgX,self.releOrgY))
-        self.turtle.reset()
-        self.turtle.penup()
-        self.turtle.speed(0)
 
         self.disp_window = Tk()
         self.listen = ButtonListeners(self)
@@ -34,100 +53,102 @@ class MyTurtle:
     def display_init(self, window, listener):
         label_x = Label(window, text="X:")
         label_y = Label(window, text="Y:")
+        label_info = Label(window, text="함수를 그려주세요. 미지수는 x만 가능")
 
         entry_x = Entry(window)
         entry_y = Entry(window)
         entry_code = Entry(window)
 
         btn_draw_point = Button(window, text="점찍기", command=listener.point)
+        btn_empty_all = Button(window, text="지우기", command=listener.empty)
         btn_func_draw = Button(window, text="함수 그리기", command=listener.draw)
 
         self.gadget_list = [
             [label_x, label_y],
             [entry_x, entry_y],
-            [btn_draw_point],
-            [entry_code, btn_func_draw]
+            [btn_draw_point, btn_empty_all],
+            [entry_code, btn_func_draw],
+            [label_info]
             ]
 
         for i in range(len(self.gadget_list)):
             for j in range(len(self.gadget_list[i])):
-                self.gadget_list[i][j].grid(row=i, column=j)
-
-    def setTurtle(self,setTurtle):
-        self.turtle = setTurtle;
-
-    def getTurtle(self):
-        return self.turtle
-
+                if i==4:
+                    self.gadget_list[i][j].grid(row=i, column=j, columnspan=2)
+                else:
+                    self.gadget_list[i][j].grid(row=i, column=j)
+                
+                    
     def drawUserFunction(self):
         exec(self.listen.code)
 
-    def drawLine(self, fromdotX, fromdotY, todotX, todotY, OrgCal):
+    def drawLine(self, turtle, fromdotX, fromdotY, todotX, todotY, OrgCal):
         if OrgCal:
-            self.turtle.goto(fromdotX+self.releOrgX, fromdotY+self.releOrgY)
-            self.turtle.pendown()
-            self.turtle.goto(todotX+self.releOrgX, todotY+self.releOrgY)
-            self.turtle.penup()
+            turtle.goto(fromdotX+self.releOrgX, fromdotY+self.releOrgY)
+            turtle.pendown()
+            turtle.goto(todotX+self.releOrgX, todotY+self.releOrgY)
+            turtle.penup()
         else:
-            self.turtle.goto(fromdotX, fromdotY)
-            self.turtle.pendown()
-            self.turtle.goto(todotX, todotY)
-            self.turtle.penup()
-            self.turtle.home()
+            turtle.goto(fromdotX, fromdotY)
+            turtle.pendown()
+            turtle.goto(todotX, todotY)
+            turtle.penup()
+            turtle.home()
 
-    def drawgoto(self, fromdotX, fromdotY):
-        self.turtle.pendown()
-        self.turtle.goto(fromdotX+self.gridOrgX, fromdotY+self.gridOrgY)
-        self.turtle.penup()
+    def drawgoto(self, turtle, fromdotX, fromdotY):
+        turtle.pendown()
+        turtle.goto(fromdotX+self.gridOrgX, fromdotY+self.gridOrgY)
+        turtle.penup()
 
-    def gotoRel(self,relX,relY):
-        self.turtle.goto(self.releOrgX + relX,self.releOrgY + relY)
+    def gotoRel(self, turtle, relX, relY):
+        turtle.goto(self.releOrgX + relX,self.releOrgY + relY)
 
-    def gotoOrg(self):
-        self.turtle.goto(self.releOrgX,self.releOrgY)
+    def gotoOrg(self, turtle):
+        turtle.goto(self.releOrgX,self.releOrgY)
 
-    def degrees(self,move):
+    def degrees(self, turtle, move):
         if move >= 360:
             move %= 360
         if move < self.degree:
-            self.turtle.right((360 - self.degree) + move)
+            turtle.right((360 - self.degree) + move)
         else:
-            self.turtle.right(move - self.degree)
+            turtle.right(move - self.degree)
         self.degree = move
 
-    def drawTextAtLeftBottomRel(self,relX,relY,text):
-        self.gotoRel(relX,relY)
-        self.degrees(45)
-        self.turtle.penup()
-        self.turtle.forward(self.gridSize/2+10)
-        self.turtle.write(text)
-        self.turtle.backward(self.gridSize/2+10)
-        self.turtle.pendown()
-        self.degrees(0)
+    def drawTextAtLeftBottomRel(self, turtle, relX, relY, text):
+        self.gotoRel(turtle, relX, relY)
+        self.degrees(turtle, 45)
+        turtle.penup()
+        turtle.forward(self.gridSize/2+10)
+        turtle.write(text)
+        turtle.backward(self.gridSize/2+10)
+        turtle.pendown()
+        self.degrees(turtle, 0)
 
-    def drawTextAtLeftBottom(self,text):
-        self.degrees(45)
-        self.turtle.penup()
-        self.turtle.forward(self.gridSize/2+10)
-        self.turtle.write(text)
-        self.turtle.backward(self.gridSize/2+10)
-        self.turtle.pendown()
-        self.degrees(0)
+    def drawTextAtLeftBottom(self, turtle, text):
+        self.degrees(turtle, 45)
+        turtle.penup()
+        turtle.forward(self.gridSize/2+10)
+        turtle.write(text)
+        turtle.backward(self.gridSize/2+10)
+        turtle.pendown()
+        self.degrees(turtle, 0)
 
     def drawFunctionY(self,func):
+        self.hold_clear = True;
         for qur in range(0, func.end_query):
             t.penup()
             for pixx in range(-self.gridSize * self.gridNormalSize, self.gridSize * self.gridNormalSize):
                 nomy = func.fy(pixx/self.gridSize)
 
-                if nomy == "complex":
+                if nomy == "complex" or nomy == "value" or nomy == "zero":
                     t.penup()
                     continue;
                 
                 pixy = nomy * self.gridSize
                 if pixy < self.gridSize * self.gridNormalSize:
                     self.listen.cord_info(pixx/self.gridSize, nomy)
-                    self.gotoRel(pixx,pixy)
+                    self.gotoRel(self.turtle, pixx, pixy)
                     t.pendown()
                 else:
                     t.penup()
@@ -137,13 +158,13 @@ class MyTurtle:
                 func.cur_query = func.cur_query + 1
             else:
                 break;
+        self.hold_clear = False;
             
 
     """def drawFunctionX(self,func):
         t.penup()
         for pixy in range(-self.gridSize * self.gridNormalSize,self.gridSize * self.gridNormalSize):
             nomx = func.fx(pixy/self.gridSize)
-
             if nomx == "complex":
                 t.penup()
                 continue;
@@ -157,23 +178,28 @@ class MyTurtle:
         t.penup()"""
             
     def drawGrid(self,num):
-        self.turtle.goto(self.releOrgX,self.releOrgY)
+        turtle = self.grid_turtle
+        turtle.goto(self.releOrgX,self.releOrgY)
         for i in range(-num+1,num):
-            self.gotoRel(-num*self.gridSize,i*self.gridSize)
-            self.drawTextAtLeftBottom(i)
-            self.drawLine(-num*self.gridSize,i*self.gridSize,num*self.gridSize,i*self.gridSize,True)
+            self.gotoRel(turtle, -num*self.gridSize,i*self.gridSize)
+            self.drawTextAtLeftBottom(turtle, i)
+            self.drawLine(turtle, -num*self.gridSize,i*self.gridSize,num*self.gridSize,i*self.gridSize,True)
             if i==0:
-                self.degrees(270)
-                self.turtle.stamp()
+                self.degrees(turtle, 270)
+                turtle.stamp()
+                self.drawTextAtLeftBottom(turtle, "x")
+                turtle.penup()
         for i in range(-num+1,num):
-            self.gotoRel(i*self.gridSize,-num*self.gridSize)
-            self.drawTextAtLeftBottom(i)
-            self.drawLine(i*self.gridSize,-num*self.gridSize,i*self.gridSize,num*self.gridSize,True)
+            self.gotoRel(turtle, i*self.gridSize,-num*self.gridSize)
+            self.drawTextAtLeftBottom(turtle, i)
+            self.drawLine(turtle, i*self.gridSize,-num*self.gridSize,i*self.gridSize,num*self.gridSize,True)
             if i==0:
-                self.degrees(180)
-                self.turtle.stamp()
-        self.gotoOrg()
-        self.drawTextAtLeftBottomRel(0,0,"O")
+                self.degrees(turtle, 180)
+                turtle.stamp()
+                self.drawTextAtLeftBottom(turtle, "y")
+                turtle.penup()
+        self.gotoOrg(turtle)
+        self.drawTextAtLeftBottomRel(turtle, 0, 0, "O")
 
 class ButtonListeners:
 
@@ -188,8 +214,12 @@ class ButtonListeners:
         lab_y = self.t.gadget_list[1][1]
         x = float(lab_x.get())
         y = float(lab_y.get())
-        self.t.getTurtle().dot()
+        self.t.turtle.dot()
         self.t.drawTextAtLeftBottom("(%.2f, %.2f)"%(x, y))
+
+    def empty(self):
+        if self.t.hold_clear != True:
+            self.t.turtle.clear()
 
     def cord_info(self, x, y):
         lab_x = self.t.gadget_list[1][0]
@@ -200,16 +230,16 @@ class ButtonListeners:
         lab_y.insert(0, "%.2f"%(y))
 
     def draw(self):
-        inst_code = """
-        local lmabda_func = lambda x : """
-        inst_code_back = """
-        self.tm.lambda_func = lmabda_func
-        self.t.drawFunctionY(self.tm)
-        """
+
+        #코드 실행기
+        inst_code = "lmabda_func = lambda x : "
+        inst_code_back ="\nself.tm.lambda_func = lmabda_func\nself.t.drawFunctionY(self.tm)"
+        
         ent_c = self.t.gadget_list[3][0]
         inst_code += ent_c.get()
         inst_code += inst_code_back
-        exec(inst_code)
+        
+        exec(compile(inst_code, "function of X->Y", "exec"))
 
 class TurtleMathFunc:
 
@@ -239,11 +269,11 @@ class TurtleMathFunc:
             #self.y = (x - 6)**2 + 5
             #self.y = math.log(x)
         except ValueError as e:
-            print(str(e))
-            return 0;
+            #print(str(e))
+            return "value";
         except ZeroDivisionError as e:
-            print(str(e))
-            return 0;
+            #print(str(e))
+            return "zero";
 
         #허수
         if self.y.__class__.__name__ == "complex":
@@ -261,17 +291,10 @@ class TurtleMathFunc:
 
 myt = MyTurtle()
 tm = TurtleMathFunc()
-t = myt.getTurtle()
+t = myt.turtle
 
 myt.listen.set_tm(tm)
 
-t.ht()
 myt.drawGrid(myt.gridNormalSize)
-
-myt.drawFunctionY(tm)
-
-lmabda_func = lambda x : 5/(x+2)-6
-
-tm.lambda_func = lmabda_func
 
 myt.drawFunctionY(tm)
